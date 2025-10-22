62815366ea71b9f72f8792010053bdb7d7af050f
unknown <holdcraftb1@udayton.edu>
2025-10-22 16:52:27 -0400

    Simulation Test File Updated with New Siumlation Controls

--- ODrive Control/ODrive simulation test.py
+++ ODrive Control/ODrive simulation test.py
@@ -2,25 +2,44 @@ import math
 import time
 
 import odrive
-from odrive.enums import AxisState, ControlMode, InputMode
-from odrive.utils import dump_errors, request_state
+from odrive.enums import *
+from odrive.utils import *
+from odrive.device_manager import *
 
 # Find a connected ODrive (this will block until you connect one)
 print("waiting for ODrive...")
-odrv0 = odrive.find_sync()
+odrv0 = odrive.find_any()
 print(f"found ODrive {odrv0._dev.serial_number}")
+
+# Clear errors
+odrv0.clear_errors()
 
 # Enter closed loop control
 odrv0.axis0.controller.config.input_mode = InputMode.PASSTHROUGH
 odrv0.axis0.controller.config.control_mode = ControlMode.POSITION_CONTROL
 request_state(odrv0.axis0, AxisState.CLOSED_LOOP_CONTROL)
 
+# Reset Current Position to 0
+odrv0.axis0.pos_estimate = 0
+
+# Live Plotter
+#start_liveplotter(properties=[odrv0.axis0._pos_estimate_property])
+
+
 # Ramp up velocity over time
 try: 
-    for i in range(100):
-        target_velocity = i * 0.1  # Increase velocity gradually
-        odrv0.axis0.controller.input_vel = target_velocity
-        time.sleep(0.1)  # Wait 100ms between updates
+    for i in range(50):
+        target_pos = i # Increase velocity gradually
+        odrv0.axis0.controller.input_pos = target_pos
+        print(target_pos)
+        time.sleep(0.1)
+        
+
+    for i in range(50, 0, -1):
+        new_target_pos = i # Increase velocity gradually
+        odrv0.axis0.controller.input_pos = new_target_pos
+        print(new_target_pos)
+        time.sleep(0.1)
 
 finally:
     request_state(odrv0.axis0, AxisState.IDLE)
